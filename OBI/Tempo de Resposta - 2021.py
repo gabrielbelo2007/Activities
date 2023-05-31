35443c69da5e569ffb17c59a7ea6a771d2dd57c4
Gabriel Belo <91219977+gabrielbelo2007@users.noreply.github.com>
2023-05-30 23:03:38 -0300

    Tempo de Resposta - progresso pt.3

--- OBI/Tempo de Resposta - 2021.py
+++ OBI/Tempo de Resposta - 2021.py
@@ -1,3 +1,5 @@
+import copy
+
 numero_registros = int(input())
 dict_registros = {}
 
@@ -6,19 +8,18 @@ tempo_amigo = 0
 
 contador = 0
 contador1 = 2
-copia_registros = []
-copia_registros_num = []
+registros = []
+registros_num = []
 
 amigos_recebidos = []
 amigos_enviados = []
 amigos_respondidos = set()
 amigos_nao_respondidos = set()
 
-
 while numero_registros > 0:
     registro = input()
-    copia_registros += [registro]
-    copia_registros_num += registro
+    registros += [registro]
+    registros_num += registro
     numero_registros -= 1
 
     chave, valor = registro.split()
@@ -60,46 +61,58 @@ if len(amigos_recebidos) != len(amigos_enviados):  # Verifica se respondeu tudo 
 else:
     amigos_respondidos = amigos_recebidos
 
+# Criar a lista com todos os amigos e seus tempos
 
+copia_registros = copy.deepcopy(registros)  # Copia de todos os registros
+lista_amigos_tempo = []
 
-x = 0
-teste = []
-z = copia_registros
-while x < 1:
-    numero_E = str(copia_registros_num[contador1])
-    tamanho_registro = len(copia_registros[0])
+while len(copia_registros) > 0:
+    letra_E = str(registros_num[contador])  # Confere a primeira letra de cada registro
+    numero_E = str(registros_num[contador1])  # Confere o número do amigo de cada registro
+    tamanho_registro = len(copia_registros[0])  # Confere o tamanho do registro, se amigo é 3 ou 30
 
-    if tamanho_registro > 3:
+    if tamanho_registro > 3:  # Caso o amigo seja com unidade e dezena
+        contador += 4
         contador1 += 1
-        numero_E += str(copia_registros_num[contador1])
+        numero_E += str(registros_num[contador1])
         contador1 += 3
+    else:  # Caso o amigo seja só unidade
+        contador += 3
+        contador1 += 3
+
+    f = "E " + numero_E  # Cria o registro de enviado com o número do amigo
+    if letra_E == "E":  # Confere se o valor que está sendo chegado é um valor de enviado
+        copia_registros.pop(0)
+        continue
+
+    descontar_T = registros_num.count("T")  # Retira os segundos adicionais quando o valor é T
+
+    contar_E = copia_registros.count(f)  # Contar quantos Enviados teve para o msm amigo
+
+    if numero_E in amigos_nao_respondidos:  # Confere se esse amigo foi respondido
+        lista_amigos_tempo += numero_E, tempo_amigo
+        if len(copia_registros) > 1:
+            copia_registros.pop(0)
+            continue
+
     else:
-        contador1 += 3
+        while contar_E > 0:  # Conta quantos segundos teve para cada amigo
+            for valor in copia_registros:
+                if valor != f:
+                    tempo_amigo += 1
+                elif valor == f and contar_E > 1:
+                    tempo_amigo += 1
+                    contar_E -= 1
+                elif contar_E == 1:
+                    contar_E -= 1
+                    break
 
-    f = "E " + numero_E
-    descontar_T = copia_registros_num.count("T")
-
-    for valor in copia_registros:
-        while valor != f:
-            tempo_amigo += 1
-            break
-        else:
+        else:  # Adiciona na lista o amigo e o seu tempo
             if len(copia_registros) > 1:
                 copia_registros.pop(0)
             tempo_amigo -= descontar_T
-            tempo_amigo += tempo_total
-    else:
-        for amigo in amigos_recebidos:
-            teste += amigo, tempo_amigo
+            tempo_amigo += tempo_total  # Precisa resolver esse tempo total !!!
+            lista_amigos_tempo += [numero_E + " " + str(tempo_amigo)]
             tempo_amigo = 0
 
-if contador > len(z):
-    x += 1
-
-
-"""
-print(dict_registros)
-print(tempo_total)
-print(amigos_recebidos, amigos_enviados)
-print(amigos_respondidos, amigos_nao_respondidos)
-"""
+print(lista_amigos_tempo)
